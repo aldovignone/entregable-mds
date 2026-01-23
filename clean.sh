536b8dede78ea8cff3844c8e981ed56ee957141d
juanRR1209 <juanrodriguez1209@gmail.com>
2026-01-23 11:34:59 -0500

    chore: eliminar husky y limpiar configuracion (#4)

--- clean.sh
+++
@@ -1,8 +0,0 @@
-rm -rf .husky
-npm uninstall husky @commitlint/cli @commitlint/config-conventional
-rm -f commitlint.config.js
-rm -f .releaserc
-rm -f .git/hooks/husky.sh
-rm -f .git/hooks/commit-msg
-rm -rf node_modules
-rm -f package-lock.json
