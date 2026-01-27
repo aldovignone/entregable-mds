f02a81dd1f4e82385788102d1a4eefa561193cdf
Aldo Vignone <aldo.vignone@students.salle.url.edu>
2026-01-27 14:19:33 +0100

    feat: return quiet hello world

--- src/controllers/helloworld.controller.ts
+++ src/controllers/helloworld.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
 
 export const getHelloWorld = (req: Request, res: Response) => {
-  res.json({ message: 'HelloWorld!' });
+  res.json({ message: 'HelloWorld' });
 };
