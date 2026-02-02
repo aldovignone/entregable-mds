9fa64a086421a8e42844795090c9ffcef39fd70f
aldovignone <aldo.vignone@students.salle.url.edu>
2026-02-02 16:55:00 +0100

    ci: run e2e tests against container before publishing a new version (#28)

---
+++ tests/e2e/routes/helloworld.routes.test.ts
@@ -0,0 +1,20 @@
+import request from 'supertest';
+
+const serverUrl = 'http://localhost:3000';
+
+describe('GET /helloworld', () => {
+  it('should return status 200', async () => {
+    const response = await request(serverUrl).get('/helloworld');
+    expect(response.status).toBe(200);
+  });
+
+  it('should return JSON content-type', async () => {
+    const response = await request(serverUrl).get('/helloworld');
+    expect(response.type).toBe('application/json');
+  });
+
+  it('should return message + data property values', async () => {
+    const response = await request(serverUrl).get('/helloworld');
+    expect(response.body).toEqual({ message: 'HelloWorld', data: { foo: 'bar' }   });
+  });
+});
