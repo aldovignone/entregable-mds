e73584b0cf5400263c92a04b56d263e17b1e85bc
Aldo Vignone <aldo.vignone@students.salle.url.edu>
2026-02-05 20:07:06 +0100

    fix: unit test

--- tests/unit/controllers/helloworld.controller.test.ts
+++ tests/unit/controllers/helloworld.controller.test.ts
@@ -16,7 +16,7 @@ describe('getHelloWorld Controller', () => {
 
   it('should call res.json with correct payload', () => {
     getHelloWorld(mockReq as Request, mockRes as Response);
-    expect(jsonSpy).toHaveBeenCalledWith({ message: 'HelloWorld!', data: { foo: 'foo' } });
+    expect(jsonSpy).toHaveBeenCalledWith({ message: 'HelloWorld', data: { foo: 'foo' } });
   });
 
   it('should call res.json exactly once', () => {
